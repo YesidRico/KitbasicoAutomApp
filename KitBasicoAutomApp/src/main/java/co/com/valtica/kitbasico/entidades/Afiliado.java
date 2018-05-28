dcd7134093b79b7c6c19b0218de7efdca6fc54af
@farid.rico <farid.rico@tecnofactor.com>
2018-05-28 09:40:31 -0500

    Inicial

---
+++ KitBasicoAutomApp/src/main/java/co/com/valtica/kitbasico/entidades/Afiliado.java
@@ -0,0 +1,70 @@
+package co.com.valtica.kitbasico.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name="afiliados")
+public class Afiliado {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idAfiliado;
+	private String numeroIdentificacion;
+	private String nombre;
+	private String apellido;
+	private String telefono;
+	private int edad;
+	
+	public Afiliado() {
+		super();
+	}
+	
+	public Afiliado(String numeroIdentificacion, String nombre, String apellido, String telefono, int edad) {
+		super();
+		this.numeroIdentificacion = numeroIdentificacion;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.telefono = telefono;
+		this.edad = edad;
+	}
+	public String getNumeroIdentificacion() {
+		return numeroIdentificacion;
+	}
+	public void setNumeroIdentificacion(String numeroIdentificacion) {
+		this.numeroIdentificacion = numeroIdentificacion;
+	}
+	public String getNombre() {
+		return nombre+"Otra versio ";
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	public boolean validar() {
+		return this.edad >= 18;
+	}
+	
+
+}
